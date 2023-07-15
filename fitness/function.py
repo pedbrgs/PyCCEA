f6d5a114cf7bd3d6592a4c9a70b0c6ecc05cad25
pedbrgs <pedbrgs@gmail.com>
2023-07-15 11:08:25 -0300

    feat: add module for fitness functions

---
+++ fitness/function.py
@@ -0,0 +1,36 @@
+import numpy as np
+from abc import ABC
+from utils.datasets import DataLoader
+from evaluation.wrapper import WrapperEvaluation
+
+
+class WrapperFitnessFunction(ABC):
+    """ An abstract class for a wrapper objective function.
+
+    It measures the quality of a solution according to the predictive performance of a machine
+    learning model.
+
+    Attributes
+    ----------
+    evaluator: object of one of the evaluation classes
+        Responsible for evaluating individuals, that is, subsets of features.   
+    """
+
+    def __init__(self, evaluator: WrapperEvaluation):
+        self.evaluator = evaluator
+
+    def _evaluate_predictive_performance(self, context_vector: np.ndarray, data: DataLoader):
+        """""
+        Evaluate predictive performance of a machine learning model trained on the selected set.
+
+        Each context vector must be represented by a binary n-dimensional array, where n is the
+        number of features. If there is a 1 in the i-th position of the array, it indicates that
+        the i-th feature should be considered and if there is a 0, it indicates that the feature
+        should not be considered.
+        """
+        evaluation = self.evaluator.evaluate(solution=context_vector.copy(),
+                                             X_train=data.X_train,
+                                             y_train=data.y_train,
+                                             X_val=data.X_val,
+                                             y_val=data.y_val)
+        return evaluation
---
+++ fitness/penalty.py
@@ -0,0 +1,58 @@
+import numpy as np
+from utils.datasets import DataLoader
+from evaluation.wrapper import WrapperEvaluation
+from fitness.function import WrapperFitnessFunction
+
+
+class SubsetSizePenalty(WrapperFitnessFunction):
+    """
+    Objective function that penalizes large subsets of features.
+
+    Rashid, A.N.M Bazlur, et al. "A novel penalty-based wrapper objective function for feature
+    selection in Big Data using cooperative co-evolution." IEEE Access 8 (2020): 150113-150129.
+
+    Attributes
+    ----------
+    w1: float
+        Predictive performance weight.
+    w2: float
+        Penalty weight.
+    """
+
+    def __init__(self, evaluator: WrapperEvaluation, weights: list):
+        super().__init__(evaluator)
+        # Check the number of weights
+        if len(weights) != 2:
+            raise AssertionError(
+                f"'{SubsetSizePenalty.__name__}' fitness function has only two components "
+                "(predictive performance and penalty). Therefore, it requires only two weights."
+            )
+        # Check the sum of the weights
+        if sum(weights) != 1:
+            raise AssertionError(
+                f"The sum of weights is {sum(weights)} but must be 1."
+            )
+        self.w1 = weights[0]
+        self.w2 = weights[1]
+
+    def evaluate(self, context_vector: np.ndarray, data: DataLoader):
+        """
+        Evaluate the given context vector using the fitness function.
+
+        Parameters
+        ----------
+        context_vector: np.ndarray
+            Solution of the complete problem.
+        data: DataLoader
+            Container with process data and training and test sets.
+
+        Returns
+        -------
+        fitness: float
+            Quality of the context vector.
+        """
+        n_features = data.X.shape[1]
+        penalty = context_vector.sum()/n_features
+        evaluation = self._evaluate_predictive_performance(context_vector, data)
+        fitness = self.w1*evaluation - self.w2*penalty
+        return fitness
