5c695b5ec3c109b93efe5158252ed1f4106b05c9
pedbrgs <pedbrgs@gmail.com>
2023-12-29 08:51:13 -0300

    chore: rename subpop variable to maintain consistency

--- cooperation/best.py
+++ cooperation/best.py
@@ -2,14 +2,12 @@ from cooperation.collaboration import Collaboration
 
 
 class SingleBestCollaboration(Collaboration):
-    """
-    Set the best individual from each subpopulation as a collaborator to any individual.
-    """
+    """Set the best individual from each subpopulation as a collaborator to any individual."""
 
     def get_collaborators(self,
                           subpop_idx: int,
                           indiv_idx: int,
-                          subpops: list,
+                          next_subpops: list,
                           current_best: dict):
         """
         Set the best individual from each subpopulation as a collaborator to the individual given
@@ -20,9 +18,11 @@ class SingleBestCollaboration(Collaboration):
         subpop_idx: int
             Index of the subpopulation to which the individual belongs.
         indiv_idx: int
-            Index of the individual in its respective subpopulation.
-        subpops: list
-            Individuals from all subpopulations.
+            Index of the individual in its respective subpopulation. The vector that represents
+            the individual is obtained from the `next_subpops`, where the individuals were evolved
+            and not evaluated.
+        next_subpops: list
+            Individuals from all subpopulations of the next generation.
         current_best: dict
             Current best individual of each subpopulation and its respective evaluation.
 
@@ -35,6 +35,6 @@ class SingleBestCollaboration(Collaboration):
         # Best individuals from each subpopulation as collaborators
         collaborators = [best["individual"] for best in current_best.values()]
         # Assign the individual itself to the subpopulation to which it belongs
-        collaborators[subpop_idx] = subpops[subpop_idx][indiv_idx].copy()
+        collaborators[subpop_idx] = next_subpops[subpop_idx][indiv_idx].copy()
 
         return collaborators
