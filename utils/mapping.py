822b13e49487415256f809e262453cb466201f3e
pedbrgs <pedbrgs@gmail.com>
2023-12-31 10:37:13 -0300

    feat: add heaviside function to map solutions from continuous to binary representation

--- utils/mapping.py
+++ utils/mapping.py
@@ -22,7 +22,28 @@ def angle_modulation_function(coeffs: np.ndarray, n_features: int) -> np.ndarray
         Binary solution in the original space.
     """
     a, b, c, d = coeffs
-    x = np.linspace(0, 1, n_features)
+    x = np.linspace(-2, 2, n_features)
     trig_function = np.sin(2 * np.pi * (x - a) * b * np.cos(2 * np.pi * (x - a) * c)) + d
     binary_solution = (trig_function > 0).astype(int)
+    return binary_solution
+
+
+def shifted_heaviside_function(real_solution: np.ndarray, shift: float = 0.5) -> np.ndarray:
+    """Convert continuous solutions into binary solutions through the heaviside step function.
+
+    Parameters
+    ----------
+    real_solution: np.ndarray (n_features,)
+        Solution with continuous values.
+    shift: float, default 0.5
+        Threshold for coding the solution from continuous-valued space to binary-valued space.
+        Each solution value will be mapped to 1 if it is greater than or equal to shift and 0
+        otherwise.
+
+    Returns
+    -------
+    binary_solution: np.ndarray (n_features,)
+        Real solution encoded in a binary representation.
+    """
+    binary_solution = (real_solution >= shift).astype(int)
     return binary_solution
