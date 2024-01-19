7a9596546311601b5457141330c1f0cac8f779eb
pedbrgs <pedbrgs@gmail.com>
2024-01-19 12:10:39 -0300

    fix: solve issue in elitism across generations

--- coevolution/ccfsrfg2.py
+++ coevolution/ccfsrfg2.py
@@ -1,6 +1,7 @@
 import gc
 import copy
 import logging
+import numpy as np
 from tqdm import tqdm
 from coevolution.ccfsrfg import CCFSRFG
 from decomposition.random import RandomFeatureGrouping
@@ -78,8 +79,12 @@ class CCFSRFG2(CCFSRFG):
             current_fitness = list()
             current_context_vectors = list()
             for i in range(self.n_subcomps):
-                current_fitness.append(list())
-                current_context_vectors.append(list())
+                # Select the 'elite_size' best individuals of the previous generation to be in the
+                # current generation (elitism)
+                descending_order = np.argsort(self.fitness[i])[::-1]
+                n_bests = descending_order[:self.optimizers[i].elite_size]
+                current_fitness.append(np.array(self.fitness[i])[n_bests].tolist().copy())
+                current_context_vectors.append(np.array(self.context_vectors[i])[n_bests].tolist().copy())
                 # Use random individuals from the previous generation as collaborators for each
                 # individual in the current generation. Except the first 'elite_size' individuals
                 # from each subpopulation which are being used as elitism and have different
@@ -111,7 +116,14 @@ class CCFSRFG2(CCFSRFG):
             )
             # Select the globally best context vector
             best_context_vector, best_fitness = self._get_global_best()
-            # Update best context vector
+
+            # In this particular case, where the problem is decomposed in each generation, the
+            # update of the best context vector and feature indices can only be done if the
+            # current fitness is greater than the best fitness and not greater than or equal to
+            # it. If we include equals in the conditional, generations in which there was no
+            # improvement will maintain the same context vector and update the feature indices
+            # incorrectly, since the elitist context vector was not necessarily generated in the
+            # current generation.
             if self.best_fitness < best_fitness:
                 # Reset stagnation counter because best fitness has improved
                 stagnation_counter = 0
