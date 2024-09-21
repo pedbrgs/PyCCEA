5722820ddd5c1d78cfcc490443dc586e7e522de3
pedbrgs@gmail.com <pedbrgs@gmail.com>
2024-09-21 12:49:51 -0300

    chore: add module init files

--- pyccea/coevolution/__init__.py
+++ pyccea/coevolution/__init__.py
@@ -1,6 +1,15 @@
-from .ccfsrfg1 import CCFSRFG1
-from .ccfsrfg2 import CCFSRFG2
-from .ccpstfg import CCPSTFG
+from .ccfc import CCFC
 from .ccsufg import CCSUFG
 from .cceafs import CCEAFS
-from .ccfc import CCFC
+from .ccpstfg import CCPSTFG
+from .ccfsrfg1 import CCFSRFG1
+from .ccfsrfg2 import CCFSRFG2
+
+__all__ = [
+    "CCFC",
+    "CCSUFG",
+    "CCEAFS",
+    "CCPSTFG",
+    "CCFSRFG1",
+    "CCFSRFG2",
+]
