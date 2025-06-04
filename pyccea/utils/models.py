b6f74db9a9d19e44df2d6101e00c044cd04df6b5
pedbrgs <pedbrgs@gmail.com>
2025-06-04 05:25:41 -0300

    chore: change some RF and CCPSTFG parameters

--- pyccea/utils/models.py
+++ pyccea/utils/models.py
@@ -206,7 +206,7 @@ class ClassificationModel():
         "k_nearest_neighbors": (KNeighborsClassifier, {"n_neighbors": 1}),
         "logistic_regression": (LogisticRegression, {}),
         "multinomial_naive_bayes": (MultinomialNB, {}),
-        "random_forest": (RandomForestClassifier, {}),
+        "random_forest": (RandomForestClassifier, {"n_jobs": -1}),
         "support_vector_machine": (SVC, {}),
     }
 
